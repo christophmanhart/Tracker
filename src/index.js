057ac56820109cfba6f76fd3eddca22d5d9931be
Benyamin Radmard <b.radmard77@gmail.com>
2021-07-10 17:33:23 +0200

    Validation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyD6mIN2RjGLihi15-W-IlWZhP8z1JDqkH8",
-    authDomain: "alkoholdatenbank.firebaseapp.com",
-    databaseURL: "https://alkoholdatenbank-default-rtdb.firebaseio.com",
-    projectId: "alkoholdatenbank",
-    storageBucket: "alkoholdatenbank.appspot.com",
-    messagingSenderId: "142784195708",
-    appId: "1:142784195708:web:e87fca0cdf84507c342df9"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 const db = firebase.firestore();
 
 
@@ -18,10 +5,7 @@ db.collection("Getraenke").get().then(value => {
 
     const ids = [
         ['bierbestand', 'biermin', 'differenzbier', 'nachkaufenbier'],
-        ['sektbestand', 'sektmin', 'differenzsekt', 'nachkaufensekt'],
         ['weinbestand', 'weinmin', 'differenzwein', 'nachkaufenwein'],
-        ['whiskybestand', 'whiskymin', 'differenzwhisky', 'nachkaufenwhisky'],
-        ['wodkabestand', 'wodkamin', 'differenzwodka', 'nachkaufenwodka'],
     ];
     const docs = value.docs;
     for (let i = 0; i < docs.length; i++) {
@@ -54,7 +38,7 @@ db.collection("Getraenke").get().then(value => {
         .then(value => {
             const datum = value.data()["Monat"];
             const heute = new Date();
-            if (datum < heute.getMonth() || (heute.getMonth() == 11 && datum == 0)) {
+            if (datum < heute.getMonth() || (heute.getMonth() == 11 && datum.getMonth() == 0)) {
                 db
                     .collection("DateCheck")
                     .doc("Date")
@@ -75,24 +59,6 @@ db.collection("Getraenke").get().then(value => {
                     .update({
                         'Gesamtverbrauch': 0
                     })
-                db
-                    .collection("Getraenke")
-                    .doc("Sekt")
-                    .update({
-                        'Gesamtverbrauch': 0
-                    })
-                db
-                    .collection("Getraenke")
-                    .doc("Whisky")
-                    .update({
-                        'Gesamtverbrauch': 0
-                    })
-                db
-                    .collection("Getraenke")
-                    .doc("Wodka")
-                    .update({
-                        'Gesamtverbrauch': 0
-                    })
             }
         });
     ermittleLiebling();
@@ -102,6 +68,12 @@ db.collection("Getraenke").get().then(value => {
         $('#buchenButton').on('click', () => {
 
             const uncheckedMenge = $('#quantity').val();
+
+            if(!isNumeric(uncheckedMenge)){
+                alert('Bitte nur Zahlen eingeben! 🤢');
+                return;
+            }
+
             let menge = 0;
             if (uncheckedMenge > 50) {
                 alert('DIGGA WAS HAST DU VOR? 🤢');
@@ -115,45 +87,6 @@ db.collection("Getraenke").get().then(value => {
 
             const kategorie = $('#getraenkeauswahl').val();
 
-            buchen(kategorie,menge);
-
-            ermittleLiebling();
-
-        });
-
-        function ermittleLiebling(){
-
-            db.collection("Getraenke").doc("Bier").get().then(value => {let bier = value.data()['Gesamtverbrauch']
-            db.collection("Getraenke").doc("Wein").get().then(value => {let wein = value.data()['Gesamtverbrauch']
-            db.collection("Getraenke").doc("Sekt").get().then(value => {let sekt = value.data()['Gesamtverbrauch']
-            db.collection("Getraenke").doc("Wodka").get().then(value => {let wodka = value.data()['Gesamtverbrauch']
-            db.collection("Getraenke").doc("Whisky").get().then(value => {let whisky = value.data()['Gesamtverbrauch']
-
-            console.log(bier,wein,sekt,whisky,wodka);
-            let all =[(bier*5),(wein*8),(sekt*12),(wodka*39),(whisky*40)];
-            let max = all.indexOf(Math.max(...all));
-
-            switch (max) {
-                case 0:
-                    document.getElementById('liebling').innerHTML = ("Bier: " + bier + "l")
-                    break;
-                case 1:
-                    document.getElementById('liebling').innerHTML = ("Wein: " + wein + "l")
-                    break;
-                case 2:
-                    document.getElementById('liebling').innerHTML = ("Sekt: " + sekt + "l")
-                    break;
-                case 3:
-                    document.getElementById('liebling').innerHTML = ("Wodka: " + wodka + "l")
-                    break;
-                case 4:
-                    document.getElementById('liebling').innerHTML = ("Whisky: " + whisky + "l")
-                    break;
-            }
-            });});});});});
-        }
-
-        function buchen(kategorie,menge){
             db
                 .collection("Getraenke")
                 .doc(kategorie)
@@ -179,6 +112,40 @@ db.collection("Getraenke").get().then(value => {
                                 'Gesamtverbrauch': aktVerbrauch + (parseInt(menge) * -1)
                             })
                     }
-        })
+            ermittleLiebling();
+
+        });
+});
+        function ermittleLiebling(){
+            var bier = -1;
+            var wein = -1
+            db
+                .collection("Getraenke")
+                .doc("Bier")
+                .get()
+                .then(value => {
+                    bier = value.data()['Gesamtverbrauch'];
+                    console.log(bier);
+
+                    db
+                        .collection("Getraenke")
+                        .doc("Wein")
+                        .get()
+                        .then(value => {
+                            wein = value.data()['Gesamtverbrauch'];
+                            console.log(wein);
+                            if((wein*8.5)<(bier*5)){
+                                document.getElementById('liebling').innerHTML = ("Bier: " + bier + "l")
+                            }
+                            else{
+                                document.getElementById('liebling').innerHTML = ("Wein: " + wein + "l")
+                            }
+                        });
+                });
         }
 
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
